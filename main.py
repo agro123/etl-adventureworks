15acc89ba2bf2fa529db823d626a347bc5f020ef
Cristian Medina <cdmedina229@gmail.com>
2025-11-29 12:43:38 -0500

    update main

--- main.py
+++ main.py
@@ -1,5 +1,216 @@
-from etl_cristian.fact_internet_sales import etl_internet_sales
-#from etl_jheison.fact_resellers_sales import etl_resellers_sales
+import pandas as pd
+from sqlalchemy import create_engine
+from sqlalchemy.engine import Engine
+import yaml
+
+from etl_cristian.utils import check_db_connection, has_new_fact_data, save_dataframe_to_csv
+from etl_cristian.extract import (
+    extract_product,
+    extract_product_subcategory,
+    extract_product_category,
+    extract_customer,
+    extract_promotion,
+    extract_currency,
+    extract_salesterritory,
+    extract_geography,
+    extract_employee,
+    extract_fact_internet_sales,
+    extract_reseller
+)
+from etl_cristian.transform import (
+    transform_product_category,
+    transform_product_subcategory,
+    transform_product,
+    transform_currency,
+    transform_promotion,
+    transform_sales_territory,
+    transform_geography,
+    transform_customer,
+    transform_employee,
+    transform_fact_internet_sales,
+    generate_dim_date,
+    transform_reseller,
+)
+from etl_cristian.load import load_table
+
+pd.set_option('display.max_rows', 100)
+pd.set_option('display.max_columns', 100)
+
+import sys
+
+def etl_dimensions(oltp_conn: Engine, olap_conn: Engine):
+    print('Extrayendo dimensiones...')
+    # Extract para DimProductCategory
+    dim_product_category = extract_product_category(oltp_conn)
+    # Extract para DimProductSubcategory
+    dim_product_subcategory = extract_product_subcategory(oltp_conn)
+    # Extract para DimProduct
+    dim_product = extract_product(oltp_conn)
+    # Extract para DimSalesTerritory
+    dim_sales_territory = extract_salesterritory(oltp_conn)
+    # Extract para DimGeography
+    dim_geography = extract_geography(oltp_conn)
+    # Extract para DimCustomer
+    dim_customer = extract_customer(oltp_conn)
+    # Extract para DimPromotion
+    dim_promotion = extract_promotion(oltp_conn)
+    # Extract para DimCurrency
+    dim_currency = extract_currency(oltp_conn)
+    # Extract para DimEmployee
+    dim_employee = extract_employee(oltp_conn)
+    # Extract para DimReseller
+    dim_reseller_raw = extract_reseller(oltp_conn)
+        
+
+    print('Transformando dimensiones...')
+    # Transform para dimProductCategory
+    dim_product_category = transform_product_category(dim_product_category)
+    # Transform para dimProductSubcategory
+    dim_product_subcategory = transform_product_subcategory(dim_product_subcategory)
+    # Transform para dimProduct
+    dim_product = transform_product(dim_product)
+    # Transform para dimSalesTerritory
+    dim_sales_territory = transform_sales_territory(dim_sales_territory)
+    # Transform para dimGeography
+    dim_geography = transform_geography(dim_geography)
+    # Transform para dimCustomer
+    dim_customer = transform_customer(dim_customer)
+    # Transform para dimPromotion
+    dim_promotion = transform_promotion(dim_promotion)
+    # Transform para dimCurrency
+    dim_currency = transform_currency(dim_currency)
+    # Transform para dimEmployee
+    dim_employee = transform_employee(dim_employee, olap_conn)
+    # Transform para dimReseller
+    dim_reseller = transform_reseller(dim_reseller_raw, olap_conn)
+
+    # Generar la dimensión de fecha
+    dim_date = generate_dim_date()
+    # Load dimDate
+    load_table(
+        dim_date,
+        olap_conn,
+        "dimdate",
+        key_columns=["datekey"],
+    )
+
+    print('Cargando dimensiones para FactInternetSales...')
+    # Load dimProductCategory
+    load_table(
+        dim_product_category,
+        olap_conn,
+        "dimproductcategory",
+        key_columns=["productcategoryalternatekey"],
+    )
+    # Load dimProductSubcategory
+    load_table(
+        dim_product_subcategory,
+        olap_conn,
+        "dimproductsubcategory",
+        key_columns=["productsubcategoryalternatekey"],
+    )
+    # Load dimProduct
+    load_table(
+        dim_product,
+        olap_conn,
+        "dimproduct",
+        key_columns=["productalternatekey"],
+    )
+    # Load dimSalesTerritory
+    load_table(
+        dim_sales_territory,
+        olap_conn,
+        "dimsalesterritory",
+        key_columns=["salesterritoryalternatekey"],
+    )
+    # Load dimGeography
+    load_table(
+        dim_geography,
+        olap_conn,
+        "dimgeography",
+        key_columns=["city", "postalcode"],
+    )
+    # Load dimCustomer
+    load_table(
+        dim_customer,
+        olap_conn,
+        "dimcustomer",
+        key_columns=["customeralternatekey"],
+    )
+    # Load dimPromotion
+    load_table(
+        dim_promotion,
+        olap_conn,
+        "dimpromotion",
+        key_columns=["promotionalternatekey"],
+    )
+    # Load dimCurrency
+    load_table(
+        dim_currency,
+        olap_conn,
+        "dimcurrency",
+        key_columns=["currencyalternatekey"],
+    )
+    # Load dimEmployee
+    load_table(
+        dim_employee,
+        olap_conn,
+        "dimemployee",
+        key_columns=["employeenationalidalternatekey"],
+    )
+    # Load dimReseller
+    load_table(
+        dim_reseller,
+        olap_conn,
+        "dimreseller",
+        key_columns=["reselleralternatekey"],
+    )
+    print('Fin carga de dimensiones...')
+
+
+def etl_internet_sales(oltp_conn: Engine, olap_conn: Engine):
+    print('Iniciando proceso ETL para FactInternetSales...')
+    # Verificar si hay nuevos datos y en ese caso iniciar la extracción
+    if has_new_fact_data(conne=olap_conn, fact_table="factinternetsales"):
+        print('Se detectaron datos nuevos en el origen. Iniciando extracción...')
+
+        print('Extrayendo información para hecho FactInternetSales...')
+        fact_internet_sales = extract_fact_internet_sales(oltp_conn)
+        print('Transformando información para hecho FactInternetSales...')
+        fact_internet_sales = transform_fact_internet_sales(fact_internet_sales, olap_conn)
+        print('Cargando hecho FactInternetSales...')
+        #save_dataframe_to_csv(fact_internet_sales, 'fact_internet_sales.csv')
+        load_table(
+            fact_internet_sales,
+            olap_conn,
+            "factinternetsales",
+            key_columns=["salesordernumber", "salesorderlinenumber"],
+        )
+    else:
+        print('No hay datos nuevos. Proceso finalizado.')
+    print('Finalizando proceso ETL para FactInternetSales...')
+    
+    return True
 
 if __name__ == "__main__":
-    etl_internet_sales()
+    with open('../config.yml', 'r') as f:
+        config = yaml.safe_load(f)
+        config_co = config['CO_SA']
+        config_olap = config['ETL_PRO']
+
+    # Construct the database URL
+    url_oltp = (f"{config_co['drivername']}://{config_co['user']}:{config_co['password']}@{config_co['host']}:"
+            f"{config_co['port']}/{config_co['dbname']}")
+    url_olap = (f"{config_olap['drivername']}://{config_olap['user']}:{config_olap['password']}@{config_olap['host']}:"
+            f"{config_olap['port']}/{config_olap['dbname']}")
+    # Create the SQLAlchemy Engine
+    oltp_conn = create_engine(url_oltp)
+    olap_conn = create_engine(url_olap)
+
+    if not check_db_connection(olap_conn, "OLAP") or not check_db_connection(oltp_conn, "OLTP"):
+        print("Unable to connect to ETL database. Exiting.")
+        sys.exit(1)
+
+    if config['LOAD_DIMENSIONS']:
+        etl_dimensions(oltp_conn, olap_conn)
+    etl_internet_sales(oltp_conn, olap_conn)
